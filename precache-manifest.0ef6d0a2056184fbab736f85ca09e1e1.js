46b2d11c2c8950fcfa18831e886ebea11d54e2da
icomgroup <105383896+icomgroup@users.noreply.github.com>
2022-05-11 20:47:19 +0300

    Add files via upload

---
+++ precache-manifest.0ef6d0a2056184fbab736f85ca09e1e1.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4de6b6fd7c742e6c2df7",
+    "url": "/app-fe/css/app.4734f479.css"
+  },
+  {
+    "revision": "719e25b3002ace53a2c8",
+    "url": "/app-fe/css/chunk-2046288d.0beebc69.css"
+  },
+  {
+    "revision": "38ee04670ef43ddef985",
+    "url": "/app-fe/css/chunk-5114b801.a81997b1.css"
+  },
+  {
+    "revision": "19a4acaa9f7f8c2ba62d",
+    "url": "/app-fe/css/chunk-58f77783.61902fd8.css"
+  },
+  {
+    "revision": "a2a108d585b9e161216e",
+    "url": "/app-fe/css/chunk-628416d2.35541f89.css"
+  },
+  {
+    "revision": "18bd93f4dd3fd0778701",
+    "url": "/app-fe/css/chunk-65a765cc.85f12bcf.css"
+  },
+  {
+    "revision": "f30fb4e96b00594cdf0c",
+    "url": "/app-fe/css/chunk-6801ce4a.acae88fe.css"
+  },
+  {
+    "revision": "4170c2b9e33912e62576",
+    "url": "/app-fe/css/chunk-b01e7ee6.13c88080.css"
+  },
+  {
+    "revision": "4d9a33eeca736ba46c0a",
+    "url": "/app-fe/css/chunk-vendors.aeff04d3.css"
+  },
+  {
+    "revision": "55c8f3ac6bcdd68a46aba8869d07c123",
+    "url": "/app-fe/img/Address.55c8f3ac.svg"
+  },
+  {
+    "revision": "356f7ac1db42b256af66cc725db16a21",
+    "url": "/app-fe/img/Callories.356f7ac1.svg"
+  },
+  {
+    "revision": "62429ff838b62b301860c319f4c6bcae",
+    "url": "/app-fe/img/Cancel.62429ff8.svg"
+  },
+  {
+    "revision": "1d4d146d9648a548148813ea84262c5e",
+    "url": "/app-fe/img/Cart.1d4d146d.svg"
+  },
+  {
+    "revision": "2c170f5de8def2700669936a3416e69c",
+    "url": "/app-fe/img/Chat.2c170f5d.svg"
+  },
+  {
+    "revision": "224dd31e5d9eaa1c9886c243589cb5b2",
+    "url": "/app-fe/img/Contact.224dd31e.svg"
+  },
+  {
+    "revision": "1350ce22e7a90020235f9bafa1a3943b",
+    "url": "/app-fe/img/Coupon.1350ce22.svg"
+  },
+  {
+    "revision": "57a72fe95a406865a8618b235342d85e",
+    "url": "/app-fe/img/Delete.57a72fe9.svg"
+  },
+  {
+    "revision": "b684e1fd3712b527f590d00f9417b72c",
+    "url": "/app-fe/img/Edit.b684e1fd.svg"
+  },
+  {
+    "revision": "f93fa85345266ae1559222e6ed5c9459",
+    "url": "/app-fe/img/History.f93fa853.svg"
+  },
+  {
+    "revision": "ed0c92c88b73ca9fe9ee63ec9e56043c",
+    "url": "/app-fe/img/Notification.ed0c92c8.svg"
+  },
+  {
+    "revision": "d6ee71937fc46c52d1e9e490855ca321",
+    "url": "/app-fe/img/Point.d6ee7193.svg"
+  },
+  {
+    "revision": "6f01525e5a24ea048d28b2ef3794e62c",
+    "url": "/app-fe/img/Promos.6f01525e.svg"
+  },
+  {
+    "revision": "a06f871d17cd5c8c43509a50f34bfc24",
+    "url": "/app-fe/img/Search.a06f871d.svg"
+  },
+  {
+    "revision": "3122b5d75ba56588373fd8368960b13e",
+    "url": "/app-fe/img/Setting.3122b5d7.svg"
+  },
+  {
+    "revision": "56b96595aec404e1eb02fc4c102bc1e4",
+    "url": "/app-fe/img/Warning.56b96595.svg"
+  },
+  {
+    "revision": "56348a855745d5290506899e8889905f",
+    "url": "/app-fe/img/Wishlist.56348a85.svg"
+  },
+  {
+    "revision": "f71559a58ec024825020ad2538937201",
+    "url": "/app-fe/img/capuccino.f71559a5.png"
+  },
+  {
+    "revision": "5c6c13a33e2442b278eabc558d68531b",
+    "url": "/app-fe/img/poster.5c6c13a3.png"
+  },
+  {
+    "revision": "8a514b82e13ff50ddae2ac4b40e741da",
+    "url": "/app-fe/index.html"
+  },
+  {
+    "revision": "4de6b6fd7c742e6c2df7",
+    "url": "/app-fe/js/app.f0bcb6a6.js"
+  },
+  {
+    "revision": "719e25b3002ace53a2c8",
+    "url": "/app-fe/js/chunk-2046288d.50609808.js"
+  },
+  {
+    "revision": "0e7cbce53e124cd43c64",
+    "url": "/app-fe/js/chunk-2d0ab565.95c9c78d.js"
+  },
+  {
+    "revision": "9d04dce2024168954456",
+    "url": "/app-fe/js/chunk-2d0af41f.70feb4af.js"
+  },
+  {
+    "revision": "dbf53aac73e20252bccb",
+    "url": "/app-fe/js/chunk-2d0e2114.8d001b77.js"
+  },
+  {
+    "revision": "38ee04670ef43ddef985",
+    "url": "/app-fe/js/chunk-5114b801.cbe5a6cb.js"
+  },
+  {
+    "revision": "19a4acaa9f7f8c2ba62d",
+    "url": "/app-fe/js/chunk-58f77783.7735ae7b.js"
+  },
+  {
+    "revision": "a2a108d585b9e161216e",
+    "url": "/app-fe/js/chunk-628416d2.616e7146.js"
+  },
+  {
+    "revision": "18bd93f4dd3fd0778701",
+    "url": "/app-fe/js/chunk-65a765cc.dc76d41f.js"
+  },
+  {
+    "revision": "f30fb4e96b00594cdf0c",
+    "url": "/app-fe/js/chunk-6801ce4a.57c3c103.js"
+  },
+  {
+    "revision": "4170c2b9e33912e62576",
+    "url": "/app-fe/js/chunk-b01e7ee6.a0d46b3d.js"
+  },
+  {
+    "revision": "4d9a33eeca736ba46c0a",
+    "url": "/app-fe/js/chunk-vendors.a2a89ec8.js"
+  },
+  {
+    "revision": "3e31ed9e255703aabbad8dfff0578e9b",
+    "url": "/app-fe/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/app-fe/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/app-fe/precache-manifest.0ef6d0a2056184fbab736f85ca09e1e1.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "heytab"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
